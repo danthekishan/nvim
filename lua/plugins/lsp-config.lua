32b34bf9aed5e9ceb1ecbea6d22cc0e1495c6aee
Daniel <daniel.abhishek002@gmail.com>
2024-11-24 17:06:17 +0530

    updates

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,70 +12,55 @@ return {
 		},
 	},
 	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+
 	-- lsp config
 	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPost", "BufNewFile" },
 		cmd = { "LspInfo", "LspInstall", "LspUninstall" },
 		dependencies = {
-			-- Automatically install LSPs and related tools to stdpath for neovim
+			-- Core LSP and installation management
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
-
-			-- Useful status updates for LSP.
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			-- UI improvements
 			{ "j-hui/fidget.nvim", opts = {} },
 		},
-		config = function()
-			--    This function gets run when an LSP attaches to a particular buffer.
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("dan-lsp-attach", { clear = true }),
-				callback = function(event)
-					-- lsp related keymaps
-					local map = function(keys, func, desc)
-						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
-
-					-- custom keymaps
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-					map("gD", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-					map("<leader>la", vim.lsp.buf.code_action, "Code [A]ction")
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-					map("K", vim.lsp.buf.hover, "Def")
-					map("ge", vim.diagnostic.open_float, "Error")
-
-					-- highlight references and remove
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-					if client and client.server_capabilities.documentHighlightProvider then
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							callback = vim.lsp.buf.clear_references,
-						})
-					end
-				end,
-			})
-
-			-- LSP servers and clients communication
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-			-- LSP servers
-			local servers = {
-
-				-- pyright
+		opts = {
+			-- Diagnostic configuration
+			diagnostics = {
+				underline = true,
+				update_in_insert = false,
+				virtual_text = {
+					spacing = 4,
+					source = "if_many",
+					prefix = "●",
+				},
+				severity_sort = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "󰅙", -- Replace with your preferred icon
+						[vim.diagnostic.severity.WARN] = "󰀦", -- Replace with your preferred icon
+						[vim.diagnostic.severity.HINT] = "󰌵", -- Replace with your preferred icon
+						[vim.diagnostic.severity.INFO] = "󰋼", -- Replace with your preferred icon
+					},
+				},
+			},
+			-- LSP feature toggles
+			inlay_hints = {
+				enabled = true,
+				exclude = { "vue" },
+			},
+			codelens = {
+				enabled = false,
+			},
+			document_highlight = {
+				enabled = true,
+			},
+			-- Server configurations
+			servers = {
+				-- Python configuration
 				pyright = {
-					-- cmd = {...},
-					-- filetyles = {...},
-					-- capabilities = {...},
 					on_attach = function(client, bufnr)
 						vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 					end,
@@ -92,57 +77,51 @@ return {
 					},
 				},
 
-				-- ruff
-				-- ruff_lsp = {
-				--   -- cmd = {...},
-				--   -- filetyles = {...},
-				--   -- capabilities = {...},
+				-- TypeScript configuration
+				-- ts_ls = {
+				-- 	alt_name = "ts_ls",
 				-- },
 
-				-- tsonsserver
-				ts_ls = {
-					-- cmd = {...},
-					-- filetyles = {...},
-					-- capabilities = {...},
-					alt_name = "ts_ls",
+				-- in favour of ts_ls
+				vtsls = {
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"javascript.jsx",
+						"typescript",
+						"typescriptreact",
+						"typescript.tsx",
+					},
+					settings = {
+						complete_function_calls = true,
+						vtsls = {
+							enableMoveToFileCodeAction = true,
+							autoUseWorkspaceTsdk = true,
+							experimental = {
+								completion = {
+									enableServerSideFuzzyMatch = true,
+								},
+							},
+						},
+						typescript = {
+							updateImportsOnFileMove = { enabled = "always" },
+							suggest = {
+								completeFunctionCalls = true,
+							},
+							inlayHints = {
+								enumMemberValues = { enabled = true },
+								functionLikeReturnTypes = { enabled = true },
+								parameterNames = { enabled = "literals" },
+								parameterTypes = { enabled = true },
+								propertyDeclarationTypes = { enabled = true },
+								variableTypes = { enabled = false },
+							},
+						},
+					},
 				},
 
-				tailwindcss = {
-					-- cmd = {...},
-					-- filetyles = {...},
-					-- capabilities = {...},
-				},
-
-				-- rust_analyzer = {
-				--   -- cmd = {...},
-				--   -- filetyles = {...},
-				--   -- capabilities = {...},
-				--   autostart = true,
-				--   -- on_attach = function(client, bufnr)
-				--   --   vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-				--   -- end,
-				--   -- capabilities = capabilities,
-				--   root_dir = function()
-				--     return vim.fn.getcwd()
-				--   end,
-				--   cmd = { "rustup", "run", "stable", "rust-analyzer" },
-				--   settings = {
-				--     rust_analyzer = {
-				--       cargo = {
-				--         allFeatures = true,
-				--       },
-				--       checkOnSave = {
-				--         command = "cargo clippy",
-				--       },
-				--     },
-				--   },
-				-- },
-
-				-- gopls
+				-- Go configuration
 				gopls = {
-					-- cmd = {...},
-					-- filetyles = {...},
-					-- capabilities = {...},
 					autostart = true,
 					on_attach = function(client, bufnr)
 						vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
@@ -158,11 +137,38 @@ return {
 					},
 				},
 
-				-- lua
+				-- HTML configuration
+				html = {
+					filetypes = { "html", "javascriptreact", "typescript.tsx", "typescriptreact" },
+					settings = {
+						html = {
+							format = {
+								templating = true,
+								wrapLineLength = 120,
+								wrapAttributes = "auto",
+							},
+							hover = {
+								documentation = true,
+								references = true,
+							},
+						},
+					},
+				},
+
+				-- CSS configuration remains the same...
+				cssls = {
+					settings = {
+						css = {
+							validate = true,
+							lint = {
+								unknownAtRules = "ignore",
+							},
+						},
+					},
+				},
+
+				-- Lua configuration
 				lua_ls = {
-					-- cmd = {...},
-					-- filetyles = {...},
-					-- capabilities = {...},
 					settings = {
 						Lua = {
 							runtime = { version = "LuaJIT" },
@@ -170,38 +176,87 @@ return {
 								checkThirdParty = false,
 								library = vim.api.nvim_get_runtime_file("lua", true),
 							},
-							-- you can toggle below to ignore Lua_ls noisy warnings
-							-- diagnostics = { disable = {"missing-fields"} },
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndex = "Disable",
+							},
 						},
 					},
 				},
-			}
-
-			-- installing mason
+			},
+		},
+		config = function(_, opts)
+			-- Setup Mason package manager
 			require("mason").setup()
 
-			-- installing mason
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
+			-- Define tools to install
+			local ensure_installed = {
 				"gopls",
 				"eslint",
 				"lua_ls",
 				"pyright",
-				"ts_ls",
+				-- "ts_ls",
+				"vtsls",
+				"html-lsp",
 				"rust_analyzer",
 				"ruff",
 				"stylua",
 				"prettier",
 				"goimports",
 				"gofumpt",
-				"tailwindcss-language-server",
 				"css-lsp",
-			})
+			}
+
+			-- Install required tools
 			require("mason-tool-installer").setup({
 				ensure_installed = ensure_installed,
 			})
 
-			-- mason lsp config
+			-- LSP attach configuration
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					-- LSP keymaps
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("gD", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					map("<leader>la", vim.lsp.buf.code_action, "Code [A]ction")
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
+					map("ge", vim.diagnostic.open_float, "Show Diagnostic")
+
+					-- Setup document highlighting
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.server_capabilities.documentHighlightProvider then
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.document_highlight,
+						})
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.clear_references,
+						})
+					end
+				end,
+			})
+
+			-- Setup LSP servers
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+			-- Configure diagnostic settings
+			vim.diagnostic.config(opts.diagnostics)
+
+			-- Setup mason-lspconfig
 			require("mason-lspconfig").setup({
 				handlers = {
 					function(server_name)
@@ -209,17 +264,23 @@ return {
 							return
 						end
 
-						local server = servers[server_name] or {}
+						local server = opts.servers[server_name] or {}
 						local name = server["alt_name"] or server_name
 
-						require("lspconfig")[name].setup({
+						local server_opts = {
 							cmd = server.cmd,
 							settings = server.settings,
 							filetypes = server.filetypes,
 							capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {}),
 							autostart = server.autostart or true,
 							on_attach = server.on_attach,
-						})
+						}
+
+						if server_name == "ts_ls" then
+							server_opts.root_dir = require("lspconfig.util").root_pattern(".git")
+						end
+
+						require("lspconfig")[name].setup(server_opts)
 					end,
 				},
 			})
@@ -230,22 +291,81 @@ return {
 	{
 		"mfussenegger/nvim-lint",
 		event = { "BufWritePost", "BufReadPost", "InsertLeave" },
-		config = function()
+		opts = {
+			-- Event to trigger linters
+			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+			linters_by_ft = {
+				fish = { "fish" },
+				python = { "ruff" },
+				javascript = { "eslint" },
+			},
+			linters = {},
+		},
+		config = function(_, opts)
+			local M = {}
 			local lint = require("lint")
-			lint.linters_by_ft = {
-				python = {
-					"ruff",
-				},
-				js = {
-					"eslint",
-				},
-			}
-			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-				group = lint_augroup,
-				callback = function()
-					require("lint").try_lint()
-				end,
+
+			-- Configure linters
+			for name, linter in pairs(opts.linters) do
+				if type(linter) == "table" and type(lint.linters[name]) == "table" then
+					lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
+					if type(linter.prepend_args) == "table" then
+						lint.linters[name].args = lint.linters[name].args or {}
+						vim.list_extend(lint.linters[name].args, linter.prepend_args)
+					end
+				else
+					lint.linters[name] = linter
+				end
+			end
+			-- Set up linters by filetype
+			lint.linters_by_ft = opts.linters_by_ft
+
+			-- Debounce function to prevent excessive linting
+			function M.debounce(ms, fn)
+				local timer = vim.uv.new_timer()
+				return function(...)
+					local argv = { ... }
+					timer:start(ms, 0, function()
+						timer:stop()
+						vim.schedule_wrap(fn)(unpack(argv))
+					end)
+				end
+			end
+
+			-- Main lint function
+			function M.lint()
+				local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+				names = vim.list_extend({}, names)
+
+				-- Add fallback linters
+				if #names == 0 then
+					vim.list_extend(names, lint.linters_by_ft["_"] or {})
+				end
+
+				-- Add global linters
+				vim.list_extend(names, lint.linters_by_ft["*"] or {})
+
+				-- Filter out invalid linters
+				local ctx = { filename = vim.api.nvim_buf_get_name(0) }
+				ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
+				names = vim.tbl_filter(function(name)
+					local linter = lint.linters[name]
+					if not linter then
+						vim.notify("Linter not found: " .. name, vim.log.levels.WARN)
+					end
+					return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
+				end, names)
+
+				-- Run linters
+				if #names > 0 then
+					lint.try_lint(names)
+				end
+			end
+
+			-- Set up autocommands
+			vim.api.nvim_create_autocmd(opts.events, {
+				group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+				callback = M.debounce(100, M.lint),
 			})
 		end,
 	},
--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,42 +1,50 @@
 return {
-  "L3MON4D3/LuaSnip",
-  build = (function()
-    -- Build Step is needed for regex support in snippets
-    -- This step is not supported in many windows environments
-    -- Remove the below condition to re-enable on windows
-    if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-      return
-    end
-    return "make install_jsregexp"
-  end)(),
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    "benfowler/telescope-luasnip.nvim",
-  },
-  config = function(_, opts)
-    local ls = require("luasnip")
-    if opts then ls.config.setup(opts) end
-    vim.tbl_map(
-      function(type) require("luasnip.loaders.from_" .. type).lazy_load() end,
-      { "vscode", "snipmate", "lua" }
-    )
-    -- friendly-snippets - enable standardized comments snippets
-    ls.filetype_extend("typescript", { "tsdoc" })
-    ls.filetype_extend("javascript", { "jsdoc", "next", "react" })
-    ls.filetype_extend("javascriptreact", { "html", "css" })
-    ls.filetype_extend("lua", { "luadoc" })
-    ls.filetype_extend("python", { "pydoc" })
-    ls.filetype_extend("rust", { "rustdoc" })
+	"L3MON4D3/LuaSnip",
+	build = (function()
+		-- Build Step is needed for regex support in snippets
+		-- This step is not supported in many windows environments
+		-- Remove the below condition to re-enable on windows
+		if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+			return
+		end
+		return "make install_jsregexp"
+	end)(),
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"benfowler/telescope-luasnip.nvim",
+	},
+	config = function(_, opts)
+		local ls = require("luasnip")
+		if opts then
+			ls.config.setup(opts)
+		end
+		vim.tbl_map(function(type)
+			require("luasnip.loaders.from_" .. type).lazy_load()
+		end, { "vscode", "snipmate", "lua" })
+		-- friendly-snippets - enable standardized comments snippets
+		ls.filetype_extend("typescript", { "tsdoc" })
+		ls.filetype_extend("javascript", { "jsdoc", "next", "react" })
+		ls.filetype_extend("javascriptreact", { "html", "css" })
+		ls.filetype_extend("typescriptreact", { "html", "css" })
+		ls.filetype_extend("lua", { "luadoc" })
+		ls.filetype_extend("python", { "pydoc" })
+		ls.filetype_extend("rust", { "rustdoc" })
 
-    -- jump forward within snippet
-    vim.keymap.set({ "i" }, "<C-k>", function() ls.expand() end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-l>", function() ls.jump(1) end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-h>", function() ls.jump(-1) end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-E>", function()
-      if ls.choice_active() then
-        ls.change_choice(1)
-      end
-    end, { silent = true })
-    vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/lua/config/snippets.lua<CR>")
-  end,
+		-- jump forward within snippet
+		vim.keymap.set({ "i" }, "<C-k>", function()
+			ls.expand()
+		end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-l>", function()
+			ls.jump(1)
+		end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-h>", function()
+			ls.jump(-1)
+		end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-E>", function()
+			if ls.choice_active() then
+				ls.change_choice(1)
+			end
+		end, { silent = true })
+		vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/lua/config/snippets.lua<CR>")
+	end,
 }
