b3f6f116ee7328e2bff4886551433a9720d8d0ba
Daniel <daniel.abhishek002@gmail.com>
2025-01-10 15:00:26 +0530

    updating blink cmp

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,103 +1,11 @@
--- return {
--- 	{
--- 		"hrsh7th/nvim-cmp",
--- 		event = "InsertEnter", -- Lazy load on entering insert mode
--- 		dependencies = {
--- 			"hrsh7th/cmp-nvim-lsp",
--- 			"hrsh7th/cmp-buffer",
--- 			"hrsh7th/cmp-path",
--- 			"L3MON4D3/LuaSnip",
--- 			"saadparwaiz1/cmp_luasnip",
--- 			{
--- 				"Saecki/crates.nvim",
--- 				event = { "BufRead Cargo.toml" },
--- 				opts = {
--- 					completion = {
--- 						cmp = { enabled = true },
--- 					},
--- 				},
--- 			},
--- 		},
---
--- 		config = function()
--- 			-- vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
--- 			local cmp = require("cmp")
--- 			local luasnip = require("luasnip")
--- 			local options = {
---
--- 				-- Snippet engine configuration
--- 				snippet = {
--- 					expand = function(args)
--- 						require("luasnip").lsp_expand(args.body)
--- 					end,
--- 				},
---
--- 				completion = { completeopt = "menu,menuone,noinsert" },
---
--- 				-- ghost text
--- 				experimental = { ghost_text = true },
---
--- 				-- window
--- 				window = {
--- 					completion = cmp.config.window.bordered(),
--- 					documentation = cmp.config.window.bordered(),
--- 				},
---
--- 				mapping = cmp.mapping.preset.insert({
---
--- 					["<C-n>"] = cmp.mapping.select_next_item(),
--- 					["<C-p>"] = cmp.mapping.select_prev_item(),
--- 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
--- 					["<C-f>"] = cmp.mapping.scroll_docs(4),
--- 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
---
--- 					-- Manually trigger a completion from nvim-cmp.
--- 					--  Generally you don't need this, because nvim-cmp will display
--- 					--  completions whenever it has completion options available.
--- 					["<C-Space>"] = cmp.mapping.complete({}),
---
--- 					-- <c-l> will move you to the right of each of the expansion locations.
--- 					-- <c-h> is similar, except moving you backwards.
--- 					["<C-l>"] = cmp.mapping(function()
--- 						if luasnip.expand_or_locally_jumpable() then
--- 							luasnip.expand_or_jump()
--- 						end
--- 					end, { "i", "s" }),
--- 					["<C-h>"] = cmp.mapping(function()
--- 						if luasnip.locally_jumpable(-1) then
--- 							luasnip.jump(-1)
--- 						end
--- 					end, { "i", "s" }),
--- 				}),
---
--- 				-- sources
--- 				sources = cmp.config.sources({
--- 					{ name = "nvim_lsp" },
--- 					{ name = "luasnip" },
--- 					{ name = "buffer" },
--- 					{ name = "lazydev", group_index = 0 },
--- 					{ name = "path" },
--- 				}),
--- 			}
---
--- 			-- `/` cmdline setup.
--- 			cmp.setup.cmdline("/", {
--- 				mapping = cmp.mapping.preset.cmdline(),
--- 				sources = {
--- 					{ name = "buffer" },
--- 				},
--- 			})
---
--- 			cmp.setup(options)
--- 		end,
--- 	},
--- }
+local trigger_text = ";"
 
 return {
 	"saghen/blink.cmp",
 	event = "InsertEnter",
 	-- optional: provides snippets for the snippet source
 	dependencies = {
+		"moyiz/blink-emoji.nvim",
 		"rafamadriz/friendly-snippets",
 		{
 			"Saecki/crates.nvim",
@@ -122,6 +30,7 @@ return {
 		},
 
 		snippets = {
+			preset = "luasnip",
 			expand = function(snippet)
 				require("luasnip").lsp_expand(snippet)
 			end,
@@ -145,7 +54,94 @@ return {
 		},
 
 		sources = {
-			default = { "lsp", "path", "luasnip", "buffer" },
+			default = { "lsp", "path", "snippets", "buffer", "emoji" },
+			providers = {
+				lsp = {
+					name = "lsp",
+					enabled = true,
+					module = "blink.cmp.sources.lsp",
+					-- kind = "LSP",
+					-- When linking markdown notes, I would get snippets and text in the
+					-- suggestions, I want those to show only if there are no LSP
+					-- suggestions
+					-- Disabling fallbacks as my snippets wouldn't show up
+					-- Enabled fallbacks as this seems to be working now
+					fallbacks = { "snippets", "buffer" },
+					score_offset = 90, -- the higher the number, the higher the priority
+				},
+				path = {
+					name = "Path",
+					module = "blink.cmp.sources.path",
+					score_offset = 25,
+					-- When typing a path, I would get snippets and text in the
+					-- suggestions, I want those to show only if there are no path
+					-- suggestions
+					fallbacks = { "snippets", "buffer" },
+					opts = {
+						trailing_slash = false,
+						label_trailing_slash = true,
+						get_cwd = function(context)
+							return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+						end,
+						show_hidden_files_by_default = true,
+					},
+				},
+				buffer = {
+					name = "Buffer",
+					enabled = true,
+					max_items = 3,
+					module = "blink.cmp.sources.buffer",
+					min_keyword_length = 4,
+					score_offset = 15, -- the higher the number, the higher the priority
+				},
+				snippets = {
+					name = "snippets",
+					enabled = true,
+					max_items = 8,
+					min_keyword_length = 2,
+					module = "blink.cmp.sources.snippets",
+					score_offset = 85, -- the higher the number, the higher the priority
+					-- Only show snippets if I type the trigger_text characters, so
+					-- to expand the "bash" snippet, if the trigger_text is ";" I have to
+					should_show_items = function()
+						local col = vim.api.nvim_win_get_cursor(0)[2]
+						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+						return before_cursor:match(trigger_text .. "%w*$") ~= nil
+					end,
+					-- After accepting the completion, delete the trigger_text characters
+					-- from the final inserted text
+					transform_items = function(_, items)
+						local col = vim.api.nvim_win_get_cursor(0)[2]
+						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+						local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
+						if trigger_pos then
+							for _, item in ipairs(items) do
+								item.textEdit = {
+									newText = item.insertText or item.label,
+									range = {
+										start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
+										["end"] = { line = vim.fn.line(".") - 1, character = col },
+									},
+								}
+							end
+						end
+						-- NOTE: After the transformation, I have to reload the luasnip source
+						-- Otherwise really crazy shit happens and I spent way too much time
+						-- figurig this out
+						vim.schedule(function()
+							require("blink.cmp").reload("snippets")
+						end)
+						return items
+					end,
+				},
+				-- https://github.com/moyiz/blink-emoji.nvim
+				emoji = {
+					module = "blink-emoji",
+					name = "Emoji",
+					score_offset = 15, -- the higher the number, the higher the priority
+					opts = { insert = true }, -- Insert emoji (default) or complete its name
+				},
+			},
 		},
 
 		signature = { enabled = true },
