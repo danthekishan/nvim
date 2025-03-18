f1d6b0541e083c4faa58599074e945741f0338df
Daniel <daniel.abhishek002@gmail.com>
2025-03-18 09:10:51 +0530

    blink update

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -10,11 +10,7 @@ return {
 		{
 			"Saecki/crates.nvim",
 			event = { "BufRead Cargo.toml" },
-			opts = {
-				completion = {
-					cmp = { enabled = true },
-				},
-			},
+			opts = {},
 		},
 	},
 	version = "*",
